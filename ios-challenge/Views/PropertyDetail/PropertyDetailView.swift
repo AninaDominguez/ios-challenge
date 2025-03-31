0f3cfed68843596f0b8ff8b8146efeb954b456df
AninaDominguez <aninadominguezz@gmail.com>
2025-03-31 22:12:41 +0200

    Updated views structure

---
+++ ios-challenge/Views/PropertyDetail/PropertyDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PropertyDetailView.swift
+//  ios-challenge
+//
+//  Created by Anina Dominguez on 31/3/25.
+//
+
+import SwiftUI
+
+struct PropertyDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PropertyDetailView()
+}
---
+++ ios-challenge/Views/PropertyDetail/PropertyDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PropertyDetailViewController.swift
+//  ios-challenge
+//
+//  Created by Anina Dominguez on 31/3/25.
+//
+
+import UIKit
+
+class PropertyDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
