6955911d758686b1c676c6b5637f62807b8b920d
AninaDominguez <aninadominguezz@gmail.com>
2025-04-01 20:26:11 +0200

    List view and viewmodel WIP

--- ios-challenge/Views/PropertiesList/PropertiesListViewController.swift
+++ ios-challenge/Views/PropertiesList/PropertiesListViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class PropertiesListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol BaseProtocol: AnyObject {
+    func showError(_ message: String)
 }
 
+class PropertiesListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var viewModel: PropertiesListViewModelProtocol?
+    private var properties: [Property] = []
+
+    public convenience init(viewModel: PropertiesListViewModelProtocol) {
+        self.init(nibName: "PropertiesListViewController", bundle: nil)
+        self.viewModel = viewModel
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Task {
+            await viewModel?.loadProperties()
+        }
+    }
+}
+// MARK: - BaseProtocol
+extension PropertiesListViewController: BaseProtocol {
+    
+    func showError(_ message: String) {
+        if !message.isEmpty {
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
+}
+
+// MARK: - TableView
+extension PropertiesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return properties.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let property = properties[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PropertyCell", for: indexPath)
+        cell.textLabel?.text = property.address
+        
+        return cell
+    }
+}
