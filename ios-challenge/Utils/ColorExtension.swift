57d6b8ab1850d91824ad71344b2a2377572ab37d
AninaDominguez <aninadominguezz@gmail.com>
2025-04-02 06:24:26 +0200

    Color extension

---
+++ ios-challenge/Utils/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  ios-challenge
+//
+//  Created by Anina Dominguez on 2/4/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundColor = Color("BackgroundColor")
+    static let borderColor = Color("BorderColor")
+    static let buttonBackgroundColor = Color("ButtonBackgroundColor")
+    static let detailsColor = Color("DetailsColor")
+    static let disabledTextColor = Color("DisabledTextColor")
+    static let primaryTextColor = Color("PrimaryTextColor")
+    static let secondaryTextColor = Color("SecondaryTextColor")
+}
